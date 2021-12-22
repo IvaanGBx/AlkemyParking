96855659bad360ccb775ca7253b35813da84db79
ivagonzalez <ivaangbx@gmail.com>
2021-12-22 09:15:55 -0500

    Comments added to ParkingSpace

--- src/main/kotlin/class/ParkingSpace.kt
+++ src/main/kotlin/class/ParkingSpace.kt
@@ -8,6 +8,8 @@ data class ParkingSpace(val vehicle:Vehicle, val parking: Parking) {
     private val parkedTime:Long
     get() = (Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis) / Constants.MINUTE_IN_MILIS
 
+
+    //`checkOutVehicle` doesn't receive `plate` as an arg because it's called from the constructor class.
     fun checkOutVehicle(){
         val vehicleCheck = parking.vehicles.firstOrNull { v -> v.plate == vehicle.plate }
         vehicleCheck?.let {
@@ -26,6 +28,11 @@ data class ParkingSpace(val vehicle:Vehicle, val parking: Parking) {
         println("Sorry, the check-out failed!")
     }
 
+
+    /*
+    * `calculateFee` doesn't receive `parkedTime` or `type` as args because it's called from the property class `parkedTime`
+    * and `type` is a property from the constructor class `vehicle`.
+    */
     private fun calculateFee() : Int{
         val baseHours = Constants.HOUR_IN_MINS * 2
         var fee = vehicle.vehicleType.price
@@ -36,9 +43,13 @@ data class ParkingSpace(val vehicle:Vehicle, val parking: Parking) {
             fee += (Constants.EXCEDENT_PRICE * blocks)
         }
 
+        //`hasDiscountCard` is not an arg because it could be accessed from the constructor `vehicle`.
+        // `discountCard` is a nullable (if null, the vehicle doesn't have discount card), so it must be validated using null safety
+        // and the scope function `let`.
         vehicle.discountCard?.let {
             fee = (fee * Constants.DISCOUNT).toInt()
         }
+
         return fee
     }
 
